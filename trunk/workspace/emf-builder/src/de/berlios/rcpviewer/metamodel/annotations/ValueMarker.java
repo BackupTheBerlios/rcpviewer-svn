2fabb66433b6bbd6b23ebf63bf4be8072f56e106
dkhaywood <dkhaywood@20aa5975-49f2-0310-8bfd-f14d13945a73>
2005-04-26 21:18:08 +0000

    subclipse seemed to have left some moved classes hanging around? deleted    rcpviewer/metamodel/annotations/Derived.java deleted    rcpviewer/metamodel/annotations/Domain.java deleted    rcpviewer/metamodel/annotations/DomainMarker.java deleted    rcpviewer/metamodel/annotations/LowerBoundOf.java deleted    rcpviewer/metamodel/annotations/Ordered.java deleted    rcpviewer/metamodel/annotations/Unique.java deleted    rcpviewer/metamodel/annotations/UpperBoundOf.java deleted    rcpviewer/metamodel/annotations/Value.java deleted    rcpviewer/metamodel/annotations/ValueMarker.java deleted    rcpviewer/metamodel/DomainClass.java deleted    rcpviewer/metamodel/ProgrammingModel.java

--- trunk/workspace/emf-builder/src/de/berlios/rcpviewer/metamodel/annotations/ValueMarker.java
+++
@@ -1,8 +0,0 @@
-package de.berlios.rcpviewer.metamodel.annotations;
-
-/**
- * Corresponds to {@link @Value} annotation.
- * 
- * TODO: Workaround since perthis() aspect matching on annotation doesn't seem to be working?
- */
-public interface ValueMarker { }
