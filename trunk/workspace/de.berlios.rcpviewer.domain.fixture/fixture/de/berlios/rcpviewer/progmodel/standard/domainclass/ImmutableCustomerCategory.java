f141e5db1a0205eeb655dbf6c25dbc53a985c77b
dkhaywood <dkhaywood@20aa5975-49f2-0310-8bfd-f14d13945a73>
2005-06-21 22:44:33 +0000

    support for @Immutable on types (exposed as IDomainObject.isChangeable())

---
+++ trunk/workspace/de.berlios.rcpviewer.domain.fixture/fixture/de/berlios/rcpviewer/progmodel/standard/domainclass/ImmutableCustomerCategory.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package de.berlios.rcpviewer.progmodel.standard.domainclass;
+
+import de.berlios.rcpviewer.progmodel.standard.Immutable;
+import de.berlios.rcpviewer.progmodel.standard.InDomain;
+
+@Immutable
+@InDomain("default")
+public class ImmutableCustomerCategory {
+
+}
---
+++ trunk/workspace/de.berlios.rcpviewer.domain.fixture/fixture/de/berlios/rcpviewer/progmodel/standard/domainclass/MutableCustomer.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package de.berlios.rcpviewer.progmodel.standard.domainclass;
+
+import de.berlios.rcpviewer.progmodel.standard.InDomain;
+
+@InDomain("default")
+public class MutableCustomer {
+
+}
