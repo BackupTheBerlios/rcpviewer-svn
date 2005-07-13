a93696db82288592bd40c3779aa6b128187d0c1a
mikee <mikee@20aa5975-49f2-0310-8bfd-f14d13945a73>
2005-07-13 08:41:46 +0000

    add further DnD types, restore gui state functionality, using annottations for parameter display

--- trunk/workspace/de.berlios.rcpviewer.gui.acme/src/de/berlios/rcpviewer/gui/acme/model/Counter.java
+++ trunk/workspace/de.berlios.rcpviewer.gui.acme/src/de/berlios/rcpviewer/gui/acme/model/Counter.java
@@ -1,5 +1,7 @@
 package de.berlios.rcpviewer.gui.acme.model;
 
+import de.berlios.rcpviewer.progmodel.extended.Named;
+import de.berlios.rcpviewer.progmodel.standard.DescribedAs;
 import de.berlios.rcpviewer.progmodel.standard.InDomain;
 
 @InDomain
@@ -15,12 +17,23 @@ public class Counter {
 		count++;
 	}
 	
-	public void increment( int inc ) {
+	public void increment( 
+			@Named("inc") //$NON-NLS-1$
+			@DescribedAs("How much to add to the counter.") //$NON-NLS-1$
+			int inc ) {
 		count = count + inc;
 	}
 	
-	public void increment( int inc1, int inc2 ) {
-		count = count + inc1 + inc2;
+	public void increment( 
+			@Named("firstInc") //$NON-NLS-1$
+			@DescribedAs("The first amount to add to the counter.") //$NON-NLS-1$
+			int firstInc, 
+			@Named("secondInc") //$NON-NLS-1$
+			@DescribedAs("The second amount to add to the counter.") //$NON-NLS-1$
+			int secondInc ) {
+		count = count + firstInc + secondInc;
 	}
+	
+
 
 }
