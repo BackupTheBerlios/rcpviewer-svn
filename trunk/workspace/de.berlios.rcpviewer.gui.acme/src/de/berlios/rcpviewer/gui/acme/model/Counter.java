606740825766b202dcbb0831e67163937fee9124
mikee <mikee@20aa5975-49f2-0310-8bfd-f14d13945a73>
2005-07-18 07:59:24 +0000

    domain object DnD

--- trunk/workspace/de.berlios.rcpviewer.gui.acme/src/de/berlios/rcpviewer/gui/acme/model/Counter.java
+++ trunk/workspace/de.berlios.rcpviewer.gui.acme/src/de/berlios/rcpviewer/gui/acme/model/Counter.java
@@ -34,6 +34,16 @@ public class Counter {
 		count = count + firstInc + secondInc;
 	}
 	
+	public void increment( 
+			@Named("bean1") //$NON-NLS-1$
+			@DescribedAs("Adds the value of field0 of an EasyBean instance to the counter.") //$NON-NLS-1$
+			EasyBean bean1,
+			@Named("bean2") //$NON-NLS-1$
+			@DescribedAs("Adds the value of field0 of an AnotherEasyBean instance to the counter.") //$NON-NLS-1$
+			AnotherEasyBean bean2 ) {
+		count = count + bean1.getField0() + bean2.getField0();
+	}
+	
 
 
 }
