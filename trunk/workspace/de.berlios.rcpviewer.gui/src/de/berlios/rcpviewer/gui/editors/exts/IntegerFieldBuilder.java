1b9efc4242454b2bf38c3b147108ec56ff39d30f
mikee <mikee@20aa5975-49f2-0310-8bfd-f14d13945a73>
2005-07-31 11:16:44 +0000

    ? Forcing overwrite of repository instance

---
+++ trunk/workspace/de.berlios.rcpviewer.gui/src/de/berlios/rcpviewer/gui/editors/exts/IntegerFieldBuilder.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package de.berlios.rcpviewer.gui.editors.exts;
+
+/**
+ * Used for <code>int</code> and <code>Integer</code>.
+ * @author Mike
+ */
+public class IntegerFieldBuilder extends AbstractPrimitiveFieldBuilder {
+
+	/**
+	 */
+	public IntegerFieldBuilder() {
+		super( int.class );
+	}
+
+}
