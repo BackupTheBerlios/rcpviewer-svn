aa06417c4a4875314496e235bc0d49cde8556396
mikee <mikee@20aa5975-49f2-0310-8bfd-f14d13945a73>
2005-08-17 11:15:04 +0000

    Beans bean with addtional references

--- trunk/workspace/de.berlios.rcpviewer.gui.acme/src/de/berlios/rcpviewer/gui/acme/model/Beans.java
+++ trunk/workspace/de.berlios.rcpviewer.gui.acme/src/de/berlios/rcpviewer/gui/acme/model/Beans.java
@@ -1,7 +1,7 @@
 package de.berlios.rcpviewer.gui.acme.model;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 
 import de.berlios.rcpviewer.progmodel.extended.Lifecycle;
 import de.berlios.rcpviewer.progmodel.standard.InDomain;
@@ -15,27 +15,45 @@ import de.berlios.rcpviewer.progmodel.standard.TypeOf;
 @InDomain
 public class Beans {
 
-	private final Collection<EasyBean> _easybeans = new ArrayList<EasyBean>();
+	private final Set<EasyBean> easybeans = new HashSet<EasyBean>();
 	private int numBeans = 0;
 	
+	private EasyBean easybean = null;
+	
 	@TypeOf( EasyBean.class )
-	public Collection<EasyBean> getEasyBeans() {
-		return _easybeans;
+	public Set<EasyBean> getEasyBeans() {
+		return easybeans;
 	}
 	
-	public void addEasyBean( EasyBean bean ) {
+	public void addToEasyBeans( EasyBean bean ) {
 		if ( bean == null ) throw new IllegalArgumentException();
-		_easybeans.add( bean );
-		numBeans = _easybeans.size();
+		easybeans.add( bean );
+		numBeans = easybeans.size();
 	}
 	
-	public void removeEasyBean( EasyBean bean ) {
+	public void removeFromEasyBeans( EasyBean bean ) {
 		if ( bean == null ) throw new IllegalArgumentException();
-		_easybeans.remove( bean );
-		numBeans = _easybeans.size();
+		easybeans.remove( bean );
+		numBeans = easybeans.size();
 	}
 	
 	public int getNumBeans() {
 		return numBeans;
 	}
+
+	/**
+	 * @return Returns the easybean.
+	 */
+	public EasyBean getEasybean() {
+		return easybean;
+	}
+
+	/**
+	 * @param easybean The easybean to set.
+	 */
+	public void setEasybean(EasyBean easybean) {
+		this.easybean = easybean;
+	}
+	
+	
 }
