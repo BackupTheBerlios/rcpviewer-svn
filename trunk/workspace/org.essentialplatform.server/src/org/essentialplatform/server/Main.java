df586dd815b0b6694fa9b07609f04d4024d123ca
dkhaywood <dkhaywood@20aa5975-49f2-0310-8bfd-f14d13945a73>
2005-12-19 22:36:33 +0000

    Initial take-on of Server plugin.

---
+++ trunk/workspace/org.essentialplatform.server/src/org/essentialplatform/server/Main.java
@@ -0,0 +1,17 @@
+package org.essentialplatform.server;
+
+/**
+ * Simple wrapper for server-side components.  
+ * 
+ * <p>
+ * For development use only.
+ * 
+ * @author Dan Haywood
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		StandaloneServer server = new StandaloneServer();
+	}
+	
+}
---
+++ trunk/workspace/org.essentialplatform.server/src/org/essentialplatform/server/StandaloneServer.java
@@ -0,0 +1,12 @@
+package org.essentialplatform.server;
+
+/**
+ * Standalone server.
+ * 
+ * @author Dan Haywood
+ *
+ */
+public class StandaloneServer {
+
+	//private 
+}
