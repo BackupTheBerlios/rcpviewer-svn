a35d0d8cda2e5be1928cd8184e087ab59fd5f15c
dkhaywood <dkhaywood@20aa5975-49f2-0310-8bfd-f14d13945a73>
2005-12-19 22:40:27 +0000

    moved to server plugin.

--- trunk/workspace/org.essentialplatform.runtime/src/org/essentialplatform/runtime/persistence/inmemory/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * An in-memory implementation of a "persistent" object store.
- * 
- * <p>
- * Objects are stored in memory; this is suitable for unit testing.
- */
-package org.essentialplatform.runtime.persistence.inmemory;
