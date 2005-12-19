b6fc6557d84c451b20c24de9b5c096ee25f03c95
dkhaywood <dkhaywood@20aa5975-49f2-0310-8bfd-f14d13945a73>
2005-12-19 22:14:47 +0000

    updates to make work with hibernate.

--- trunk/examples/workspace/demo/src/demo/Customer.java
+++ trunk/examples/workspace/demo/src/demo/Customer.java
@@ -1,30 +1,48 @@
 package demo;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-import org.essentialplatform.progmodel.essential.app.IPrerequisites;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratorType;
+import javax.persistence.Transient;
+
+import org.essentialplatform.core.domain.Domain;
+import org.essentialplatform.progmodel.essential.app.AssignmentType;
+import org.essentialplatform.progmodel.essential.app.Id;
 import org.essentialplatform.progmodel.essential.app.InDomain;
 import org.essentialplatform.progmodel.essential.app.Lifecycle;
 import org.essentialplatform.progmodel.essential.app.Named;
-import org.essentialplatform.progmodel.essential.app.Prerequisites;
-import org.essentialplatform.progmodel.essential.app.Programmatic;
 import org.essentialplatform.progmodel.essential.app.RelativeOrder;
 import org.essentialplatform.progmodel.essential.app.TypeOf;
-
-import org.essentialplatform.core.domain.Domain;
 import org.essentialplatform.runtime.domain.IDomainObject;
 import org.essentialplatform.runtime.session.ISession;
-import org.essentialplatform.runtime.session.Session;
 import org.essentialplatform.runtime.session.SessionManager;
+import org.essentialplatform.runtime.transaction.ITransactable;
+import org.essentialplatform.runtime.transaction.ITransaction;
 
 @Lifecycle(instantiable=true)
 @InDomain
+@javax.persistence.Entity(access=AccessType.PROPERTY)
 public class Customer {
 
 	
-	private String firstName;
+	private Integer id;
 	@RelativeOrder(1)
+	@Id(assignedBy=AssignmentType.OBJECT_STORE)
+	@javax.persistence.Id(generate=GeneratorType.AUTO)
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
+	private String firstName;
+	@RelativeOrder(2)
 	public String getFirstName() {
 		return firstName;
 	}
@@ -33,7 +51,7 @@ public class Customer {
 	}
 
 	private String middleInitial;
-	@RelativeOrder(2)
+	@RelativeOrder(3)
 	public String getMiddleInitial() {
 		return middleInitial;
 	}
@@ -41,17 +59,9 @@ public class Customer {
 		this.middleInitial = middleInitial;
 	}
 	
-	private int age;
-	@RelativeOrder(4)
-	public int getAge() {
-		return age;
-	}
-	public void setAge(int age) {
-		this.age = age;
-	}
 	
 	private String lastName;
-	@RelativeOrder(3)
+	@RelativeOrder(4)
 	public String getLastName() {
 		return lastName;
 	}
@@ -59,47 +69,68 @@ public class Customer {
 		this.lastName = lastName;
 	}
 	
-	
 	public void changeName(
 			@Named("Last name")
 			final String lastName) {
 		this.lastName = lastName;
 	}
 
-
+	
+	private int age;
+	@RelativeOrder(5)
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
 	
 	private List<Order> orders = new ArrayList<Order>();
 	@TypeOf(Order.class)
+	@RelativeOrder(6)
+	@javax.persistence.OneToMany(mappedBy="customer")
+	@javax.persistence.OrderBy("date")
 	public List<Order> getOrders() {
 		return orders;
+	}
+	/**
+	 * Immutable.
+	 * 
+	 * <p>
+	 * Setter is required for object store (<tt>private</tt> visibility is 
+	 * sufficient).
+	 * 
+	 * @param customer
+	 */
+	private void setOrders(List<Order> orders) {
+		this.orders = orders;
 	}
 	private void addToOrders(final Order order) {
 		orders.add(order);
 	}
 	
 	
-	public Order placeOrder() {
-		// following instantiates an Order
-		// yes, it's horrible, but will be replaced by a simple pointcut on {@InDomain).new
-		ISession session = SessionManager.instance().get(SessionManager.instance().getCurrentSessionId());
-		IDomainObject<Order> orderDo = session.create(Domain.lookupAny(Order.class));
-		Order order = orderDo.getPojo();
+	public Order placeOrder(String description) {
+		Order order = Order.create(this, description);
 		addToOrders(order);
 		return order;
 	}
-//	public Order placeOrder(Product p) {
-//		// following instantiates an Order
-//		// yes, it's horrible, but will be replaced by a simple pointcut on {@InDomain).new
-//		ISession session = SessionManager.instance().get(SessionManager.instance().getCurrentSessionId());
-//		IDomainObject<Order> orderDo = session.create(RuntimeDomain.lookupAny(Order.class));
-//		Order order = orderDo.getPojo();
-//		order.setProduct(p);
-//		addToOrders(order);
-//		return order;
-//	}
+
+	/**
+	 * Factory method.
+	 * 
+	 * @param customer
+	 * @return
+	 */
+	public static Customer create() {
+		// following instantiates an Customer
+		// yes, it's horrible, but will be replaced by a simple pointcut on {@InDomain).new
+		ISession session = SessionManager.instance().get(SessionManager.instance().getCurrentSessionId());
+		IDomainObject<Customer> customerDo = session.create(Domain.lookupAny(Customer.class));
+		Customer customer = customerDo.getPojo();
+		return customer;
+	}
 	
-	
-	
-	
-	
+
 }
--- trunk/examples/workspace/demo/src/demo/Order.java
+++ trunk/examples/workspace/demo/src/demo/Order.java
@@ -2,16 +2,94 @@ package demo;
 
 import java.util.Date;
 
+import javax.persistence.AccessType;
+import javax.persistence.CascadeType;
+import javax.persistence.GeneratorType;
+
+import org.essentialplatform.core.domain.Domain;
+import org.essentialplatform.progmodel.essential.app.AssignmentType;
+import org.essentialplatform.progmodel.essential.app.Id;
 import org.essentialplatform.progmodel.essential.app.InDomain;
+import org.essentialplatform.progmodel.essential.app.Invisible;
 import org.essentialplatform.progmodel.essential.app.Lifecycle;
 import org.essentialplatform.progmodel.essential.app.Named;
+import org.essentialplatform.progmodel.essential.app.RelativeOrder;
+import org.essentialplatform.runtime.domain.IDomainObject;
+import org.essentialplatform.runtime.session.ISession;
+import org.essentialplatform.runtime.session.SessionManager;
 
 @Lifecycle(instantiable=true)
 @InDomain
+@javax.persistence.Entity(access=AccessType.PROPERTY)
+@javax.persistence.Table(name="MyOrder")
 public class Order {
 
 	
+	
+	private Integer id;
+	@Invisible
+	@Id(assignedBy=AssignmentType.OBJECT_STORE)
+	@javax.persistence.Id(generate=GeneratorType.AUTO)
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	
+	
+	private Customer customer;
+	@RelativeOrder(1)
+	@javax.persistence.ManyToOne(cascade={CascadeType.ALL})
+	public Customer getCustomer() {
+		return customer;
+	}
+	/**
+	 * Immutable.
+	 * 
+	 * <p>
+	 * Setter is required for object store (<tt>private</tt> visibility is 
+	 * sufficient).
+	 * 
+	 * @param customer
+	 */
+	private void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	
+	
+	private Date date;
+	@RelativeOrder(2)
+	public Date getDate() {
+		return date;
+	}
+	/**
+	 * Immutable.
+	 * 
+	 * <p>
+	 * Setter is required for object store (<tt>private</tt> visibility is 
+	 * sufficient).
+	 * 
+	 * @param customer
+	 */
+	private void setDate(Date date) {
+		this.date = date;
+	}
+	
+
+	private int quantity;
+	@RelativeOrder(3)
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
 	private String description;
+	@RelativeOrder(4)
 	public String getDescription() {
 		return description;
 	}
@@ -21,22 +99,25 @@ public class Order {
 	
 	
 	
-	private int quantity;
-	public int getQuantity() {
-		return quantity;
-	}
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
+	/**
+	 * Factory method.
+	 * 
+	 * @param customer
+	 * @return
+	 */
+	static Order create(final Customer customer, final String description) {
+		// following instantiates an Order
+		// yes, it's horrible, but will be replaced by a simple pointcut on {@InDomain).new
+		ISession session = SessionManager.instance().get(SessionManager.instance().getCurrentSessionId());
+		IDomainObject<Order> orderDo = session.create(Domain.lookupAny(Order.class));
+		Order order = orderDo.getPojo();
+		order.customer = customer;
+		order.date = new Date();
+		order.setDescription(description);
+		return order;
 	}
 	
-	
-	
-	private Date date;
-	public Date getDate() {
-		return date;
-	}
-	public void setDate(Date date) {
-		this.date = date;
-	}
-	
+	// static Order create(final Customer customer, Product p, final String description) {
+
+
 }
