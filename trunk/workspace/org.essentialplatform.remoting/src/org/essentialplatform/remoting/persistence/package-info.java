69d53c6cad2713197c4571c32c7cb5dc90c44553
dkhaywood <dkhaywood@20aa5975-49f2-0310-8bfd-f14d13945a73>
2005-12-19 22:26:01 +0000

    Added persistence package.

---
+++ trunk/workspace/org.essentialplatform.remoting/src/org/essentialplatform/remoting/persistence/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Facades that represent the persistent object store.
+ * 
+ * <p>
+ * For example, client- and server-side repositories.
+ */
+package org.essentialplatform.remoting.persistence;
