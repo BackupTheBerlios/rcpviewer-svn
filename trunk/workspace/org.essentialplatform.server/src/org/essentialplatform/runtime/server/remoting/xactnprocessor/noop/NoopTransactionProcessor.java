b1a33b7227d05c7d86d9845684ca2b5e89b0fbcd
dkhaywood <dkhaywood@20aa5975-49f2-0310-8bfd-f14d13945a73>
2006-01-04 00:16:23 +0000

    attempting to move classes about a bit

---
+++ trunk/workspace/org.essentialplatform.server/src/org/essentialplatform/runtime/server/remoting/xactnprocessor/noop/NoopTransactionProcessor.java
@@ -0,0 +1,8 @@
+package org.essentialplatform.runtime.server.remoting.xactnprocessor.noop;
+
+import org.essentialplatform.runtime.server.remoting.xactnprocessor.AbstractTransactionProcessor;
+
+public final class NoopTransactionProcessor extends AbstractTransactionProcessor {
+
+
+}
