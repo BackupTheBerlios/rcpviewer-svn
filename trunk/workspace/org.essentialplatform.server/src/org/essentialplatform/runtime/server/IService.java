e46c6ef67abac10ac4c4a9ea3a6ee417aadf05c0
dkhaywood <dkhaywood@20aa5975-49f2-0310-8bfd-f14d13945a73>
2006-01-04 00:09:59 +0000

    attempting to move classes about a bit

---
+++ trunk/workspace/org.essentialplatform.server/src/org/essentialplatform/runtime/server/IService.java
@@ -0,0 +1,10 @@
+package org.essentialplatform.runtime.server;
+
+public interface IService {
+
+	public boolean isStarted();
+	
+	public void start()  throws IllegalStateException;
+	public void shutdown() throws IllegalStateException;
+
+}
