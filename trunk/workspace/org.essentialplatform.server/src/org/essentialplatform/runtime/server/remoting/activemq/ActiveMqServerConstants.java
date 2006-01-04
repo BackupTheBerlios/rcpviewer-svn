efd9553bd1486b7048c594fdd27e73bfb3cb4c61
dkhaywood <dkhaywood@20aa5975-49f2-0310-8bfd-f14d13945a73>
2006-01-04 00:13:50 +0000

    attempting to move classes about a bit

---
+++ trunk/workspace/org.essentialplatform.server/src/org/essentialplatform/runtime/server/remoting/activemq/ActiveMqServerConstants.java
@@ -0,0 +1,8 @@
+package org.essentialplatform.runtime.server.remoting.activemq;
+
+public final class ActiveMqServerConstants {
+	public static final String TRANSACTION_QUEUE = "org.essentialplatform.test";
+
+	private ActiveMqServerConstants(){}
+
+}
